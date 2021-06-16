059aa96420ef92e7104bae46b3ce4d6ad877e9bb
Blurryface <1592077291@qq.com>
2021-06-16 12:54:40 +0800

    first commit

---
+++ src/cn/JavaAPIDemo.java
@@ -0,0 +1,11 @@
+package cn;
+
+public class JavaAPIDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Runtime runtime=Runtime.getRuntime();
+		System.out.println(runtime.availableProcessors());
+	}
+
+}
